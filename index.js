04acc6a26a58959dcbaf655613dedb50f1e58ce3
fechtech <fechtech@anonyme.org>
2014-12-21 11:26:27 +0000

    correction

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var data = express.readFileSync('index.html')
+  var fs = require('fs')
+  var data = fs.readFileSync('index.html')
   response.send(data.toString('utf-8'))
 })
 
